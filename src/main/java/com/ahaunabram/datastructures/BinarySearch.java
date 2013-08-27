284251bafdb5fbe9804d5cf69aded72d6fad06ff
Shaun Abram <shaun.abram@blackrock.com>
2013-08-27 16:22:27 -0700

    Added BinarySearch algos + tests

---
+++ src/main/java/com/ahaunabram/datastructures/BinarySearch.java
@@ -0,0 +1,66 @@
+package com.ahaunabram.datastructures;
+
+import java.util.Arrays;
+
+/**
+ * PROBLEM Implement a function to perform a binary search on a sorted array of integers to
+ * find the index of a given integer.
+ *
+ * Comment on the efficiency of this search:
+ *      Worst case performance is log n (where log is binary log, i.e. log base 2)
+ * compare it with other search methods
+ *
+ */
+public class BinarySearch {
+
+
+    public static int recursive(int[] ar, int target) {
+        int lower = 0;
+        int upper = ar.length-1;
+        while (lower <= upper) {
+            int candidateIndex = mid(lower, upper);
+            int candidate = ar[candidateIndex];
+            if (candidate < target) {
+                lower = candidateIndex + 1;
+                int[] newar = Arrays.copyOfRange(ar, lower, upper + 1);
+                recursive(newar, target);
+            } else if (candidate > target) {
+                upper = candidateIndex - 1;
+                int[] newar = Arrays.copyOfRange(ar, lower, upper + 1);
+                recursive(newar, target);
+            } else {
+                return candidateIndex;
+            }
+        }
+        return -1;
+    }
+
+    public static int iterative(int[] ar, int target) {
+        int lower = 0;
+        int upper = ar.length-1;
+        while ((lower <= upper) ) {
+            int candidateIndex = mid(lower, upper);
+            int candidate = ar[candidateIndex];
+            if (candidate < target) {
+                lower = candidateIndex+1;
+            } else if (candidate > target) {
+                upper = candidateIndex-1;
+            } else {
+                return candidateIndex;
+            }
+
+        }
+        return -1;
+    }
+
+    public static int sys(int[] ar, int target) {
+        return Arrays.binarySearch(ar, target);
+    }
+
+    public static int mid(int lower, int upper) {
+        //surprisingly, this may not work:
+        //  (lower+upper)/2;
+        //see http://googleresearch.blogspot.com/2006/06/extra-extra-read-all-about-it-nearly.html
+        return (lower + upper) >>> 1;
+    }
+}
---
+++ src/test/java/com/ahaunabram/datastructures/BinarySearchTest.java
@@ -0,0 +1,85 @@
+package com.ahaunabram.datastructures;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class BinarySearchTest {
+
+    @Test
+    public void bsearch_returns_correct_index_for_single_element_array() {
+        int[] sortedIntArray = {100};
+        int index = BinarySearch.recursive(sortedIntArray, 100);
+        assertEquals(0, index);
+    }
+
+    @Test
+    public void bsearch_returns_correct_index_when_finding_first_element_in_double_element_array() {
+        int[] sortedIntArray = {100, 200};
+        int index = BinarySearch.recursive(sortedIntArray, 100);
+        assertEquals(0, index);
+    }
+
+    @Test
+        public void bsearch_returns_correct_index_when_finding_last_element_in_double_element_array() {
+            int[] sortedIntArray = {100, 200};
+            int index = BinarySearch.recursive(sortedIntArray, 200);
+            assertEquals(1, index);
+        }
+
+    @Test
+    public void bsearch_returns_correct_index_when_finding_middle_in_array() {
+        int[] sortedIntArray = {0,1,2};
+        int index = BinarySearch.recursive(sortedIntArray, 2);
+        assertEquals(2, index);
+    }
+
+    @Test
+    public void bsearch_returns_correct_index_when_finding_last_in_array() {
+        int[] sortedIntArray = {0,5,10,15,20,25};
+        int index = BinarySearch.recursive(sortedIntArray, 25);
+        assertEquals(5, index);
+    }
+
+    @Test
+    public void bsearch_returns_correct_index_when_finding_fist_in_array() {
+        int[] sortedIntArray = {0,5,10,15,20,25};
+        int index = BinarySearch.recursive(sortedIntArray, 0);
+        assertEquals(0, index);
+    }
+
+    @Test
+    public void bsearch_returns_negative_when_target_not_found() {
+        int[] sortedIntArray = {0,1,2};
+        int index = BinarySearch.recursive(sortedIntArray, 4);
+        /* My implementations return -1;
+           Arrays.binarySearch returns (-(insertion point) - 1)
+         */
+        assertTrue(index < 0);
+    }
+
+    @Test
+    public void mid_picks_middle() {
+        assertEquals(2, BinarySearch.mid(1, 3));
+    }
+
+    @Test
+    public void mid_pick_lower_when_upper_eqs_lowerPlusOne() {
+        //when upper = lower + 1
+        //mid can be either upper or lower? Just do consistently.
+        //we always do on the lower side.
+        assertEquals(1, BinarySearch.mid(1, 2));
+    }
+
+    @Test
+    public void mid_picks_lower_when_upper_eqs_lower() {
+        assertEquals(1, BinarySearch.mid(1, 1));
+    }
+
+    @Test
+    public void mid_handles_large_numbers() {
+        int expected = Integer.MAX_VALUE -1 ;
+        assertEquals(expected, BinarySearch.mid(Integer.MAX_VALUE-1, Integer.MAX_VALUE));
+    }
+}
