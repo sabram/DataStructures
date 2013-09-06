883823877dc4fc9a05f210e6d873bb5198689ab6
Shaun Abram <shaun.abram@blackrock.com>
2013-09-06 16:22:56 -0700

    Initial working version of QuickSort

---
+++ src/main/java/com/ahaunabram/datastructures/Quicksort.java
@@ -0,0 +1,65 @@
+package com.ahaunabram.datastructures;
+
+import java.util.Arrays;
+
+public class Quicksort {
+    public static int[] quicksort(int[] input) {
+        return pivot(input);
+    }
+
+    private static int[] pivot(int[] input) {
+        if (input.length == 1) return input;
+        int pivot = selectPivot(input);
+        int i=1;
+        int j=1;
+        while (j<input.length) {
+            int next = input[j];
+            if (next < pivot) {
+                swap(input, i, j);
+                i++;
+            }
+            j++;
+        }
+        //swap(input, 0, i);
+        int[] lower = {};
+        if (i>1) {
+            lower = Arrays.copyOfRange(input, 1, i);
+            lower = pivot(lower);
+        }
+        int[] upper = {};
+        if ((j-i)>0) {
+            upper = Arrays.copyOfRange(input, i, input.length);
+            upper = pivot(upper);
+        }
+        return merge(lower, pivot, upper);
+    }
+
+    private static int[] merge(int[] lower, int pivot, int[] upper) {
+        int[] merged = new int[lower.length + upper.length + 1];
+        int i = 0;
+        while (i<merged.length) {
+            if (lower.length>0) {
+                for (int lowerNum : lower) {
+                    merged[i++] = lowerNum;
+                }
+            }
+            merged[i++] = pivot;
+            if (upper.length>0) {
+                for (int upperNum : upper) {
+                    merged[i++] = upperNum;
+                }
+            }
+        }
+        return merged;
+    }
+
+    private static void swap(int[] input, int i, int j) {
+        int tmp = input[i];
+        input[i] = input[j];
+        input[j] = tmp;
+    }
+
+    private static int selectPivot(int[] unsorted) {
+        return unsorted[0];
+    }
+}
---
+++ src/test/java/com/ahaunabram/datastructures/QuickSortTest.java
@@ -0,0 +1,37 @@
+package com.ahaunabram.datastructures;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class QuickSortTest {
+    
+    @Test
+        public void quicksort_sorts_simple_unsorted_list() {
+            int[] unsorted = {5,2};
+            int[] sorted = {2,5};
+            assertThat(Quicksort.quicksort(unsorted)).isEqualTo(sorted);
+        }
+    
+        @Test
+        public void quicksort_sorts_unsorted_list() {
+            int[] unsorted = {5,2,9,1,8,6,4,3,7};
+            int[] sorted = {1,2,3,4,5,6,7,8,9};
+            assertThat(Quicksort.quicksort(unsorted)).isEqualTo(sorted);
+        }
+    
+        @Test
+        public void quicksort_sorts_simple_unbalanced_unsorted_list() {
+            int[] unsorted = {2,2,2,1,1};
+            int[] sorted = {1,1,2,2,2};
+            assertThat(Quicksort.quicksort(unsorted)).isEqualTo(sorted);
+        }
+    
+        @Test
+        public void quicksort_sorts_unbalanced_unsorted_list() {
+            int[] unsorted = {2,2,2,2,2,1,1,1,1,1};
+            int[] sorted = {1,1,1,1,1,2,2,2,2,2};
+            assertThat(Quicksort.quicksort(unsorted)).isEqualTo(sorted);
+        }
+    
+}
