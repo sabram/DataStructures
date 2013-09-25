8f356f03ecd6f2759e30d3da6bf944f66ae8ad15
Shaun Abram <shaunabram@gmail.com>
2013-09-25 13:06:56 -0700

    Fibonacci

---
+++ src/main/java/com/ahaunabram/datastructures/Fibonacci.java
@@ -0,0 +1,37 @@
+package com.ahaunabram.datastructures;
+
+public class Fibonacci {
+
+    public static int fibRecursive(int i) {
+        if (i==0) return 0;
+        if (i==1) return 1;
+        else return fibRecursive(i-1) + fibRecursive(i-2);
+    }
+
+    public static int fibRecursiveWithCaching(int i) {
+        int[] cache = new int[i];
+        if (i==0) return 0;
+        if (i==1) return 1;
+        int arIndex = i-1;
+        if (cache[arIndex] != 0) return cache[arIndex];
+        else {
+            int fib = fibRecursive(i-1) + fibRecursive(i-2);
+            cache[arIndex] = fib;
+            return fib;
+        }
+    }
+
+    public static int fib(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+        int fibN = 0;
+        int fibNMinusOne = 1;
+        int fibNMinusTwo = 0;
+        for (int j=2; j<=n; j++) {
+            fibN = fibNMinusOne + fibNMinusTwo;
+            fibNMinusTwo = fibNMinusOne;
+            fibNMinusOne = fibN;
+        }
+        return fibN;
+    }
+}
---
+++ src/test/java/com/ahaunabram/datastructures/FibonacciTest.java
@@ -0,0 +1,30 @@
+package com.ahaunabram.datastructures;
+
+import com.google.common.collect.Lists;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class FibonacciTest {
+
+    List<Integer> fibResults;
+
+    @Before
+    public void setup() {
+        fibResults = Lists.newArrayList(0,1,1,2,3,5,8,13,21,34);
+    }
+    @Test
+    public void fib() {
+        for (int i=0; i<10; i++) {
+            int expected = fibResults.get(i);
+            int actual = Fibonacci.fib(i);
+            assertThat(actual)
+                    .overridingErrorMessage(i + " Fib should be " + expected + " but got " + actual)
+                    .isEqualTo(expected);
+        }
+
+    }
+}
