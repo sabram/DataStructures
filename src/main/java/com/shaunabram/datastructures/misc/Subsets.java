02c8e182c13b062e310df270bc9197d1f7e542e0
Shaun Abram <shaunabram@gmail.com>
2013-10-08 07:02:59 -0700

    removed commented code

--- src/main/java/com/shaunabram/datastructures/misc/Subsets.java
+++ src/main/java/com/shaunabram/datastructures/misc/Subsets.java
@@ -7,49 +7,6 @@ public class Subsets {
 
     public static final Set<Integer> EMPTY_SET = new HashSet<>();
 
-//    public static Set<Set<Integer>> findSubsets(Set<Integer> s) {
-//        System.out.println("Finding sunsets for " + s);
-//        if (s==null) return null;
-//        Set<Set<Integer>> allSubsets = new HashSet<>();
-//        if (s.size()==0) return allSubsets;
-//        s.remove(EMPTY_SET);
-//        if (s.size() >= 1) {
-//            allSubsets.add(s);
-//            for (int index=0; index<s.size(); index++) {
-//                Set<Integer> subsetMinusOne = new HashSet<>(s);
-//                Integer first = removeOne(subsetMinusOne, index);
-//                Set<Set<Integer>> subsets = findSubsets(subsetMinusOne);
-//                allSubsets.addAll(subsets);
-//                for (Set<Integer> i : subsets) {
-//                    Set<Integer> newSet = new HashSet<>();
-//                    newSet.add(first);
-//                    newSet.addAll(i);
-//                    allSubsets.addAll(subsets);
-//                }
-//                Set<Integer> setWithJustFirst = new HashSet<>();
-//                setWithJustFirst.add(first);
-//                allSubsets.add(setWithJustFirst);
-//            }
-//
-//        }
-//        allSubsets.add(EMPTY_SET);
-//        return allSubsets;
-//    }
-//
-//    private static Integer removeOne(Set<Integer> set, int i) {
-//        Iterator<Integer> iterator = set.iterator();
-//        int index = 0;
-//        Integer next = null;
-//        while (iterator.hasNext()) {
-//            next = iterator.next();
-//            if (i==index) {
-//                set.remove(next);
-//                break;
-//            }
-//        }
-//        return next;
-//    }
-
     public static Set<Set<Integer>> findSubsets(Set<Integer> set) {
         if (set==null) return null;
         double expectedSubsetsSize = Math.pow(2, set.size());
