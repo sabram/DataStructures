84643724841bf2779b8bf2e06c50d2c71814dabd
Shaun Abram <shaun.abram@blackrock.com>
2013-10-16 08:09:40 -0700

    Updated MaxProfits to use MinMaxPriorityQueue

--- src/main/java/com/shaunabram/datastructures/misc/MaxProfits.java
+++ src/main/java/com/shaunabram/datastructures/misc/MaxProfits.java
@@ -1,7 +1,6 @@
 package com.shaunabram.datastructures.misc;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
+import com.google.common.collect.MinMaxPriorityQueue;
 
 /**
  *  I have an array stockPricesYesterday where the keys are the number of minutes
@@ -15,37 +14,30 @@ import java.util.Queue;
 public class MaxProfits {
 
     public static int getMaxProfit(Integer[] stockPrices) {
-        Queue<Integer> maxPricesHeap = createHeapOfPrices(stockPrices);
+        MinMaxPriorityQueue<Integer> maxPricesHeap = createHeapOfPrices(stockPrices);
 
         int maxProfit = 0;
         for (Integer buyPrice : stockPrices) {
-            Integer sellPrice = getMaxPrice(maxPricesHeap);
+            Integer sellPrice = maxPricesHeap.peekLast();
             int tmpMaxProfit = sellPrice - buyPrice;
             if (tmpMaxProfit > maxProfit) {
                 maxProfit = tmpMaxProfit;
             }
-            removePrice(maxPricesHeap, buyPrice);
+            maxPricesHeap.remove(buyPrice);
         }
         if (maxProfit<0) maxProfit = 0;
         return maxProfit;
     }
 
-    private static boolean removePrice(Queue<Integer> maxPricesHeap, Integer buyPrice) {
-        return maxPricesHeap.remove(buyPrice * -1);
-    }
-
-    private static Integer getMaxPrice(Queue<Integer> maxPricesHeap) {
-        return -1 * maxPricesHeap.peek();
-    }
-
     /**
      * Note that since head of a PriorityQueue is the min element, and
-     * we want the max, we are storing all numbers are -ve
+     * we want the max, we are using a MinMaxPriorityQueue, which
+     * provides both peek() and peekLast() in constant time.
      */
-    private static Queue<Integer> createHeapOfPrices(Integer[] stockPrices) {
-        Queue<Integer> maxPricesHeap = new PriorityQueue<>();
+    private static MinMaxPriorityQueue<Integer> createHeapOfPrices(Integer[] stockPrices) {
+        MinMaxPriorityQueue<Integer> maxPricesHeap = MinMaxPriorityQueue.create();
         for (Integer stockPrice : stockPrices) {
-            maxPricesHeap.add(-1 * stockPrice);
+            maxPricesHeap.add(stockPrice);
         }
         return maxPricesHeap;
     }
