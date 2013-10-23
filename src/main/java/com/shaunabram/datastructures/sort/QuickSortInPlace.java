eef1fa65b098dd64726090923f8ace83dfd5a05e
Shaun Abram <shaun.abram@blackrock.com>
2013-10-23 15:05:59 -0700

    Added placeholder and notes for better pivot selection

--- src/main/java/com/shaunabram/datastructures/sort/QuickSortInPlace.java
+++ src/main/java/com/shaunabram/datastructures/sort/QuickSortInPlace.java
@@ -3,16 +3,35 @@ package com.shaunabram.datastructures.sort;
 public class QuickSortInPlace {
     public static int[] quicksort(int[] input) {
         if (input == null || input.length <= 1) return input;
-        return pivot(input, 0, input.length);
+        return quicksort(input, 0, input.length);
     }
 
-    private static int[] pivot(int[] input, int lower, int upper) {
-        if (upper-lower<=1) return input;
-        int pivot = selectPivot(input, lower);
+    private static int[] quicksort(int[] input, int lowerIdx, int upperIdx) {
+        if (upperIdx-lowerIdx<=1) return input;
 
-        int i=lower+1;
+        //for simplicity, we just use the first element as the pivot
+        int pivotIdx = lowerIdx;
+
+        //But if we didn't use first element as the pivot,
+        //this algo would still work as long as we move the pivot to the start.
+        //For example
+        //int pivotIdx = selectPivot(input, lowerIdx, upperIdx);
+        //swap(input, lowerIdx, pivotIdx);
+
+        pivotIdx = pivot(input, lowerIdx, upperIdx, pivotIdx);
+
+        //recursively sort the array the left and right of the pivot
+        quicksort(input, lowerIdx, pivotIdx);
+        quicksort(input, pivotIdx + 1, input.length);
+
+        return input;
+    }
+
+    private static int pivot(int[] input, int lowerIdx, int upperIdx, int pivotIdx) {
+        int pivot = input[pivotIdx];
+        int i=lowerIdx+1;
         int j=i;
-        while (j<upper) {
+        while (j<upperIdx) {
             int next = input[j];
             if (next < pivot) {
                 swap(input, i, j);
@@ -22,15 +41,10 @@ public class QuickSortInPlace {
         }
 
         //put pivot in its rightful place
-        //i.e. swap pivot last 'less than' number (lower)
-        int pivotIdx = i-1;
-        swap(input, lower, pivotIdx);
+        //i.e. swap pivot (at lowerIdx) with the last 'less than' number (at i-1)
+        swap(input, lowerIdx, i-1);
 
-        //recursively sort the array the left and right of the pivot
-        pivot(input, lower, pivotIdx);
-        pivot(input, pivotIdx+1, input.length);
-
-        return input;
+        return i-1;
     }
 
     private static void swap(int[] input, int i, int j) {
@@ -39,7 +53,9 @@ public class QuickSortInPlace {
         input[j] = tmp;
     }
 
-    private static int selectPivot(int[] input, int lower) {
-        return input[lower];
+    @SuppressWarnings("unused")
+    //unused but left as a placeholder for improving pivot selection
+    private static int selectPivot(int[] input, int lower, int upper) {
+        return lower;
     }
 }
